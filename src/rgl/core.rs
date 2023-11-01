3679e8289be0878a7b8aaf084db8897906184dfd
ink0rr <tomflynnbeltsazar@gmail.com>
2023-11-01 10:36:00 +0700

    Create data folder if not exists

--- src/rgl/core.rs
+++ src/rgl/core.rs
@@ -1,7 +1,7 @@
 use super::{copy_dir, empty_dir, export_project, find_temp_dir, symlink, Config, RunContext};
 use crate::{info, warn};
 use anyhow::{Context, Result};
-use std::io;
+use std::{fs, io};
 
 pub fn run_or_watch(profile_name: &str, watch: bool) -> Result<()> {
     let config = Config::load()?;
@@ -14,12 +14,12 @@ pub fn run_or_watch(profile_name: &str, watch: bool) -> Result<()> {
     copy_dir(&context.behavior_pack, temp.join("BP"))?;
     copy_dir(&context.resource_pack, temp.join("RP"))?;
     if let Err(e) = symlink(&context.data_path, temp.join("data")) {
-        match e.downcast_ref::<io::Error>() {
-            Some(io_error) => match io_error.kind() {
-                io::ErrorKind::NotFound => warn!("Data folder does not exists"),
-                _ => return Err(e),
-            },
-            None => return Err(e),
+        match e.downcast_ref::<io::Error>().map(|e| e.kind()) {
+            Some(io::ErrorKind::NotFound) => {
+                warn!("Data folder does not exists, creating...");
+                fs::create_dir_all(&context.data_path)?;
+            }
+            _ => return Err(e),
         }
     }
 
