46d12a7c59d6f50e6b929c6ef2c97a7e02477889
ink0rr <tomflynnbeltsazar@gmail.com>
2023-11-15 22:07:31 +0700

    Refactor file system functions

--- src/rgl/file_system.rs
+++ src/rgl/file_system.rs
@@ -1,7 +1,7 @@
 use anyhow::{anyhow, Context, Result};
 use dunce::canonicalize;
 use rayon::prelude::*;
-use std::{fs, path::Path};
+use std::{fs, io, path::Path};
 
 fn copy_dir_impl(from: &Path, to: &Path) -> Result<()> {
     fs::create_dir_all(to)?;
@@ -33,57 +33,59 @@ pub fn copy_dir(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
     ))
 }
 
-fn empty_dir_impl(path: impl AsRef<Path>) -> Result<()> {
-    rimraf(&path).map_err(|e| anyhow!("{}", e.root_cause()))?;
-    fs::create_dir_all(&path)?;
+fn empty_dir_impl(path: &Path) -> Result<()> {
+    rimraf(path).map_err(|e| anyhow!("{}", e.root_cause()))?;
+    fs::create_dir_all(path)?;
     Ok(())
 }
 
 pub fn empty_dir(path: impl AsRef<Path>) -> Result<()> {
-    empty_dir_impl(&path).context(format!(
+    let path = path.as_ref();
+    empty_dir_impl(path).context(format!(
         "Failed to empty directory\n\
          <yellow> >></> Path: {}",
-        path.as_ref().display(),
+        path.display(),
     ))
 }
 
 pub fn move_dir(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
-    fs::rename(&from, &to).context(format!(
+    let from = from.as_ref();
+    let to = to.as_ref();
+    fs::rename(from, to).context(format!(
         "Failed to move directory\n\
          <yellow> >></> From: {}\n\
          <yellow> >></> To: {}",
-        from.as_ref().display(),
-        to.as_ref().display(),
+        from.display(),
+        to.display(),
     ))
-}
-
-fn read_json_impl<T>(path: impl AsRef<Path>) -> Result<T>
-where
-    T: serde::de::DeserializeOwned,
-{
-    let data = fs::read_to_string(&path)?;
-    let json = serde_json::from_str(&data)?;
-    Ok(json)
 }
 
 pub fn read_json<T>(path: impl AsRef<Path>) -> Result<T>
 where
     T: serde::de::DeserializeOwned,
 {
-    read_json_impl(&path).context(format!(
+    let inner = || -> Result<T> {
+        let data = fs::read_to_string(&path)?;
+        let json = serde_json::from_str(&data)?;
+        Ok(json)
+    };
+    inner().context(format!(
         "Failed to read JSON file {}",
         path.as_ref().display()
     ))
 }
 
 fn rimraf_impl(path: &Path) -> Result<()> {
-    let _ = fs::read_dir(path)?
+    fs::read_dir(path)?
         .par_bridge()
         .map(|entry| -> Result<()> {
             let entry = entry?;
             let path = entry.path();
-            if entry.metadata()?.is_dir() {
+            let metadata = entry.metadata()?;
+            if metadata.is_dir() {
                 rimraf_impl(&path)?;
+            } else if cfg!(windows) && metadata.is_symlink() {
+                fs::remove_dir(path)?;
             } else {
                 fs::remove_file(path)?;
             }
@@ -106,58 +108,52 @@ pub fn rimraf(path: impl AsRef<Path>) -> Result<()> {
     Ok(())
 }
 
-#[cfg(any(target_os = "linux", target_os = "macos"))]
-pub fn symlink(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
+#[cfg(unix)]
+fn symlink_impl(from: &Path, to: &Path) -> io::Result<()> {
     use std::os::unix;
-
-    let from = canonicalize(&from)?;
-    unix::fs::symlink(&from, &to).context(format!(
-        "Failed to create symlink\n\
-         <yellow> >></> From: {}\n\
-         <yellow> >></> To: {}",
-        from.display(),
-        to.as_ref().display()
-    ))
+    unix::fs::symlink(from, to)
 }
 
-#[cfg(target_os = "windows")]
-pub fn symlink(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
+#[cfg(windows)]
+fn symlink_impl(from: &Path, to: &Path) -> io::Result<()> {
     use std::os::windows;
+    windows::fs::symlink_dir(from, to)
+}
 
-    let from = canonicalize(&from)?;
-    windows::fs::symlink_dir(&from, &to).context(format!(
+pub fn symlink(from: impl AsRef<Path>, to: impl AsRef<Path>) -> Result<()> {
+    let from = canonicalize(from)?;
+    let to = to.as_ref();
+    symlink_impl(&from, to).context(format!(
         "Failed to create symlink\n\
          <yellow> >></> From: {}\n\
          <yellow> >></> To: {}",
         from.display(),
-        to.as_ref().display()
+        to.display()
     ))
 }
 
 pub fn write_file<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> Result<()> {
-    fs::write(&path, contents).context(format!(
+    let path = path.as_ref();
+    fs::write(path, contents).context(format!(
         "Failed to write file\n\
          <yellow> >></> Path: {}",
-        path.as_ref().display()
+        path.display()
     ))
-}
-
-fn write_json_impl<T>(path: impl AsRef<Path>, data: &T) -> Result<()>
-where
-    T: serde::Serialize,
-{
-    let data = serde_json::to_string_pretty(data)?;
-    write_file(&path, data + "\n")?;
-    Ok(())
 }
 
 pub fn write_json<T>(path: impl AsRef<Path>, data: &T) -> Result<()>
 where
     T: serde::Serialize,
 {
-    write_json_impl(&path, data).context(format!(
+    let path = path.as_ref();
+    let inner = || -> Result<()> {
+        let data = serde_json::to_string_pretty(data)?;
+        write_file(path, data + "\n")?;
+        Ok(())
+    };
+    inner().context(format!(
         "Failed to write JSON file\n\
          <yellow> >></> Path: {}",
-        path.as_ref().display()
+        path.display()
     ))
 }
