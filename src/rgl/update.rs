81ab9e5d9b893bae88bc4467b1ea826e197a42f6
ink0rr <tomflynnbeltsazar@gmail.com>
2023-11-16 02:09:48 +0700

    feat: replace reqwest with ureq

--- src/rgl/update.rs
+++ src/rgl/update.rs
@@ -37,9 +37,10 @@ fn update_timestamp() -> Result<()> {
 }
 
 fn get_latest_version() -> Result<String> {
-    let version = reqwest::blocking::get(CARGO_URL)
+    let version = ureq::get(CARGO_URL)
+        .call()
         .context("Failed to fetch Cargo.toml")?
-        .text()
+        .into_string()
         .context("Failed to parse Cargo.toml")?
         .lines()
         .find(|line| line.starts_with("version = \""))
@@ -54,10 +55,12 @@ fn download_pkg(url: &str) -> Result<Vec<u8>> {
     let mut logger = paris::Logger::new();
     logger.loading(format!("Downloading {url}"));
 
-    let res = reqwest::blocking::get(url)?;
+    let mut reader = ureq::get(url).call()?.into_reader();
+    let mut bytes = Vec::new();
+    reader.read_to_end(&mut bytes)?;
 
     logger.log(format!("<green>[SUCCESS]</> Downloaded {url}"));
-    Ok(res.bytes()?.into())
+    Ok(bytes)
 }
 
 fn extract_pkg(bytes: Vec<u8>, path: &Path) -> Result<PathBuf> {
