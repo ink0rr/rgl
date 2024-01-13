e64ef0643b5d70793b67727193ff8ad70009c384
ink0rr <tomflynnbeltsazar@gmail.com>
2024-01-13 20:02:01 +0700

    chore: deprecate `rgl install` and `rgl uninstall` command

--- src/commands/mod.rs
+++ src/commands/mod.rs
@@ -1,7 +1,7 @@
 mod add;
 mod clean;
+mod get;
 mod init;
-mod install;
 mod list;
 mod remove;
 mod run;
@@ -9,8 +9,8 @@ mod update;
 
 pub use self::add::*;
 pub use self::clean::*;
+pub use self::get::*;
 pub use self::init::*;
-pub use self::install::*;
 pub use self::list::*;
 pub use self::remove::*;
 pub use self::run::*;
