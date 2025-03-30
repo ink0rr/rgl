15d8fdf0a5a2e753e669b73bde7fbc10756c7cbb
ink0rr <tomflynnbeltsazar@gmail.com>
2025-03-31 02:05:14 +0700

    fix: replace cmd with powershell

--- src/rgl/filter_shell.rs
+++ src/rgl/filter_shell.rs
@@ -10,9 +10,10 @@ pub struct FilterShell {
 
 impl Filter for FilterShell {
     fn run(&self, context: &FilterContext, temp: &Path, run_args: &[String]) -> Result<()> {
-        let cmd = if cfg!(windows) { "cmd" } else { "sh" };
-        Subprocess::new(cmd)
-            .args(&["-c", &self.command])
+        let shell = if cfg!(windows) { "powershell" } else { "sh" };
+        Subprocess::new(shell)
+            .arg("-c")
+            .arg(&self.command)
             .args(run_args)
             .current_dir(temp)
             .setup_env(&context.filter_dir)?
